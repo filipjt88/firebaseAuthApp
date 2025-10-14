a4ff1a7a563d32ef6634190e8aad292a49fa1042
filipjt88 <filip88ks@gmail.com>
2025-10-14 21:51:15 +0200

    add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,2 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9mExampleKey123456",
+  authDomain: "firebaseauthapp-12345.firebaseapp.com",
+  projectId: "firebaseauthapp-12345",
+  storageBucket: "firebaseauthapp-12345.appspot.com",
+  messagingSenderId: "9876543210",
+  appId: "1:9876543210:web:abcdef123456",
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
