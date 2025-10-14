785e51cbb0ab53435de81484ebe8cf9a29e83080
filipjt88 <filip88ks@gmail.com>
2025-10-14 22:05:07 +0200

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
-    <h1>Firebase Auth app</h1>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h2>Welcome to Firebase Auth app</h2>
+}
+
+export default Home;
