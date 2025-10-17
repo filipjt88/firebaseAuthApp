6e2e7e4ca5fc67c66f6a9a168256e8beccbc3a07
filipjt88 <filip88ks@gmail.com>
2025-10-17 23:17:06 +0200

    register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { register } from "../services/auth";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
-    return <h2>Register page</h2>
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try {
+            await register(email,password);
+            navigate("/");
+        }catch(error) {
+            alert(error.message);
+        }
+    };
+
+    return(
+         <form onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button type="submit">Register</button>
+    </form>
+    );
+};
 
 export default Register;
