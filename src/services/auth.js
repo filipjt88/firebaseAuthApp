74c5d6c515246cdd0604d2d80a5f4ca742cab34b
filipjt88 <filip88ks@gmail.com>
2025-10-17 22:56:10 +0200

    add services login and register function

---
+++ src/services/auth.js
@@ -0,0 +1,19 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "./firebase";
+
+// Register
+export const register = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email,password);
+};
+
+// Login
+export const login = (email,password) => {
+    return signInWithEmailAndPassword(email,password);
+};
+
+export const logout = () => {
+    return signOut(auth);
+}
+
+
+
